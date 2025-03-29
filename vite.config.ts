fa941bd7419ac3569d89c1ace5660a27360ea61e
Kwazi <kwazinkosizwane@gmail.com>
2025-03-29 23:32:38 +0200

    Added .env file

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,5 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
-  },
-  define: {
-    VITE_SUPABASE_URL: JSON.stringify("https://zwgudiwlrrufkequijug.supabase.co"),
-    VITE_SUPABASE_ANON_KEY: JSON.stringify("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp3Z3VkaXdscnJ1ZmtlcXVpanVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5MjUwNjcsImV4cCI6MjA1ODUwMTA2N30.wYiqYeYWQgYC02BS7MWN-xwG_xLkC7ulpeEadZXbRek"),
-  },
+  }
 });
